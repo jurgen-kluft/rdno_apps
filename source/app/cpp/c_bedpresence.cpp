812f3013f492f57657832a38753d49a69bb0ff3e
jurgen.kluft <jurgen.kluft@gmail.com>
2025-05-23 15:40:18 +0800

    * rdno_bedpresence
    * wifi connected, client connected, packets are send

--- source/app/cpp/c_bedpresence.cpp
+++ source/app/cpp/c_bedpresence.cpp
@@ -6,93 +6,108 @@
 #include "rdno_wifi/c_client.h"
 #include "rdno_core/c_timer.h"
 #include "rdno_core/c_serial.h"
+#include "rdno_core/c_sensor_packet.h"
 
-#include "rdno_bedpresence/c_network_secrets.h"
-
-namespace ncore
-{
-    static byte           sLocalMemory[1024];  // Local memory for the linear allocator
-    static linear_alloc_t gLinearAlloc;
-
-};  // namespace ncore
+#include "rdno_bedpresence/c_network.secret.h"
 
 using namespace ncore;
 
-static s16                    gClientIndex = -1;  // Client index for the TCP client
-static nwifi::nstatus::enum_t gWifiStatus  = nwifi::nstatus::Idle;
+static s16               gClientIndex  = -1;                   // Client index for the TCP client
+static nstatus::status_t gClientStatus = nstatus::Idle;        //
+static nstatus::status_t gWifiStatus   = nstatus::Idle;        //
+static const char*       gHostName     = "BedPresenceDevice";  // Hostname for the device
+
+// TODO; we need a way to provide a 'logger', so that in the final build we can
+//       disable the serial output, and use a different method to log messages.
 
 // Initialize the system
 void setup()
 {
-    gLinearAlloc.setup(sLocalMemory, sizeof(sLocalMemory));
+    nserial::Begin(ncore::nserial::nbaud::Rate115200);  // Initialize serial communication at 115200 baud
 
     // Initialize the WiFi module
+    nwifi::ConfigIpAddrNone();
+    nwifi::SetHostname(gHostName);
     nwifi::BeginEncrypted(WIFI_SSID, WIFI_PASSWORD);  // Connect to the WiFi network with SSID "OBNOSIS8"
 
-    // Create a new client and connect to the server
-    gClientIndex = nclient::NewClient(&gLinearAlloc);
-    s32 error    = nclient::Connect(gClientIndex, SERVER_IP, SERVER_PORT);  // Connect to the server
+    gWifiStatus = nwifi::Status();  // Get the current WiFi status
+    if (gWifiStatus == nstatus::Connected)
+    {
+        nserial::Println("Connected to WiFi ...");
+    }
+
+    // Connect client to connect to the server
+    gClientIndex  = 0;
+    gClientStatus = nclient::Connect(gClientIndex, SERVER_IP, SERVER_PORT);  // Connect to the server
 
     // This is where you would set up your hardware, peripherals, etc.
-    // TODO; we need a way to provide a 'logger', so that in the final build we can
-    //       disable the serial output, and use a different method to log messages.
-    ncore::nserial::Begin(ncore::nserial::nbaud::Rate115200);  // Initialize serial communication at 115200 baud
-
-    ncore::npin::SetPinMode(2, ncore::npin::ModeOutput);  // Set the LED pin as output
+    npin::SetPinMode(2, ncore::npin::ModeOutput);  // Set the LED pin as output
 
     nserial::Println("Bed Presence ...");
 }
 
 // Constants
-const float V_REF     = 5.0;                // Analog reference voltage (e.g., 5V or 3.3V)
-const s32   R_BITS    = 10;                 // ADC resolution (bits)
-const float ADC_STEPS = (1 << R_BITS) - 1;  // Number of steps (2^R_BITS - 1)
+const s32 R_BITS    = 10;                          // ADC resolution (bits)
+const f32 ADC_STEPS = 1.0f / ((1 << R_BITS) - 1);  // Number of steps (2^R_BITS - 1)
 
 const u8 kLeftSideGPIO  = 10;  // Pin number for the left side sensor
 const u8 kRightSideGPIO = 12;  // Pin number for the right side sensor
 
-// Convert ADC value to voltage
-// Formula: voltage = (adc_value / ADC_STEPS) * V_REF
-inline float to_voltage(s32 adc_value) { return (static_cast<float>(adc_value) / ADC_STEPS) * V_REF; }
+// Convert ADC value to a value between 0.0 and 1.0
+// Formula: presence = (adc_value / ADC_STEPS)
+inline float ToPresence(s32 adc_value) { return (static_cast<float>(adc_value) * ADC_STEPS); }
+
+static nsensor::SensorPacket_t gSensorPacket;  // Sensor packet for sending data
+static u16                     gSequence = 0;  // Sequence number for the packet
+static const u8                kVersion  = 1;  // Version number for the packet
 
 // Main loop of the application
 void loop()
 {
-    s32 left_side   = nadc::analogRead(kLeftSideGPIO);  // Read the left side sensor value
-    s32 left_side_v = to_voltage(left_side);            // Convert the sensor value to voltage
-
-    s32 right_side   = nadc::analogRead(kRightSideGPIO);  // Read the right side sensor value
-    s32 right_side_v = to_voltage(right_side);            // Convert the sensor value to voltage
-
-    nwifi::nstatus::enum_t wifi_status = nwifi::Status();  // Get the current WiFi status
-    if (wifi_status == nwifi::nstatus::Connected)
+    gWifiStatus = nwifi::Status();  // Get the current WiFi status
+    if (gWifiStatus == nstatus::Connected)
     {
-        if (nclient::Connected(gClientIndex))
+        nserial::Println("[Loop] Connected to WiFi ...");
+
+        gClientStatus = nclient::Connected(gClientIndex);
+        if (gClientStatus == nstatus::Connected)
         {
+            nserial::Println("[Loop] Connected to Server ...");
+
+            const s32 left_side          = nadc::AnalogRead(kLeftSideGPIO);  // Read the left side sensor value
+            const f32 left_side_presence = ToPresence(left_side);            // Convert the sensor value to voltage
+
+            const s32 right_side          = nadc::AnalogRead(kRightSideGPIO);  // Read the right side sensor value
+            const s32 right_side_presence = ToPresence(right_side);            // Convert the sensor value to voltage
+
             // Write a custom (binary-format) network message
-            nclient::Write(gClientIndex, left_side_v);   // Send the left side voltage to the server
-            nclient::Write(gClientIndex, right_side_v);  // Send the right side voltage to the server
+            gSensorPacket.begin(gSequence++, kVersion);
+            gSensorPacket.write_info(nsensor::DeviceLocation::Bedroom1, nsensor::DeviceLabel::BedPresence);                                                    // Write the header for the left side sensor
+            gSensorPacket.write_sensor_value(nsensor::SensorType::Presence, nsensor::SensorChannel::Channel0, nsensor::SensorState::On, left_side_presence);   // Write the left side sensor value
+            gSensorPacket.write_sensor_value(nsensor::SensorType::Presence, nsensor::SensorChannel::Channel1, nsensor::SensorState::On, right_side_presence);  // Write the left side sensor value
+            gSensorPacket.finalize();
+
+            nclient::Write(gClientIndex, gSensorPacket.Data, gSensorPacket.Size);  // Send the sensor packet to the server
         }
         else
-        {
-            // If the client is not connected, try to reconnect
-            nclient::Connect(gClientIndex, SERVER_IP, SERVER_PORT);  // Reconnect to the server
-            ntimer::Delay(1000);                                     // Wait for 1 second
+        {  // If the client is not connected, try to reconnect
+            nserial::Println("[Loop] Connecting to server ...");
+            gClientStatus = nclient::Connect(gClientIndex, SERVER_IP, SERVER_PORT, 5000);  // Reconnect to the server (already has timeout=5 seconds)
         }
     }
     else
     {
-        // If the client is not connected, try to reconnect
-        nwifi::BeginEncrypted(WIFI_SSID, WIFI_PASSWORD);
-        ntimer::Delay(1000);  // Wait for 1 second
+        ntimer::Delay(2000);  // Wait for 2 seconds before trying to reconnect
+        // nwifi::Reconnect();   // Reconnect to the WiFi network
     }
-}
 
+    ntimer::Delay(10000);  // Wait for 10 seconds
+}
 
 #ifndef TARGET_ESP32
 
-// We need a 'void main()' function to run the code on non-ESP32 platforms
-#include "rdno_core/c_setup_loop.h"
+    // We need a 'void main()' function to run the code on non-ESP32 platforms
+    #include "rdno_core/c_setup_loop.h"
 
 int main()
 {
